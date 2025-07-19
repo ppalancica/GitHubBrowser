3eeb87c643415386b7b85e44e6ac41a75d96754e
Pavel Palancica <pavel.palancica@gmail.com>
2025-07-19 22:17:49 +0300

    Fixed a NavBar issue

--- GitHubBrowser/Screens/FollowersVC.swift
+++ GitHubBrowser/Screens/FollowersVC.swift
@@ -15,7 +15,12 @@ final class FollowersVC: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
